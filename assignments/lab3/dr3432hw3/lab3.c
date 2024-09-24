fb7f7d7b6fdb8c7470bd037b1d28769fbbd750ec
DiegoRos <diegorosenberg@gmail.com>
2024-09-24 19:04:52 -0400

    Error in printing jiffies time difference at exit

--- assignments/lab3/dr3432hw3/lab3.c
+++ assignments/lab3/dr3432hw3/lab3.c
@@ -8,13 +8,16 @@ unsigned long jiffiesStartTimeStamp;
 static int hello_init(void){
 	jiffiesStartTimeStamp = jiffies;
 	unsigned long msClockSpeed = 1000 / HZ;
-	printk(KERN_ALERT "Hello, world. Tick Time: %li\n", msClockSpeed);
+	printk(KERN_ALERT "Hello, world. Clock speed (ms): %li\n", msClockSpeed);
+	printk(KERN_ALERT "Tick Time: %li\n", jiffiesStartTimeStamp);
 	return 0;
 }
 
 static void hello_exit(void){
+	unsigned long jiffiesEndTimeStamp = jiffies;
+	unsigned long jiffiesTimeTakenMiliseconds = 1000 * (jiffiesEndTimeStamp -jiffiesStartTimeStamp) / HZ;
 	printk(KERN_ALERT "Goodbye, cruel world\n");
-	printk(KERN_ALERT "Jiffies Time: %li",(unsigned long) (jiffies - jiffiesStartTimeStamp) / HZ);
+	printk(KERN_ALERT "Jiffies Time: %li", jiffiesTimeTakenMiliseconds);
 }
 
 module_init(hello_init);
