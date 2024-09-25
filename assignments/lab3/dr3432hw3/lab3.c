b48f1f404afcf9b3f4eefb1fbbaf1d905e28d6ff
DiegoRos <diegorosenberg@gmail.com>
2024-09-25 13:02:20 -0400

    Completed jiffies part of question two but missing correction on ktime implementation.

--- assignments/lab3/dr3432hw3/lab3.c
+++ assignments/lab3/dr3432hw3/lab3.c
@@ -1,23 +1,28 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/jiffies.h>
+#include <linux/timekeeping.h>
+#include <linux/ktime.h>
 
 MODULE_LICENSE("Dual DSD/GPL");
 unsigned long jiffiesStartTimeStamp;
+s64 kTimeStartTimeStamp;
 
 static int hello_init(void){
 	jiffiesStartTimeStamp = jiffies;
+	kTimeStartTimeStamp = ktime_to_ms(ktime_get_boottime());
 	unsigned long msClockSpeed = 1000 / HZ;
 	printk(KERN_ALERT "Hello, world. Clock speed (ms): %li\n", msClockSpeed);
-	printk(KERN_ALERT "Tick Time: %li\n", jiffiesStartTimeStamp);
 	return 0;
 }
 
 static void hello_exit(void){
-	unsigned long jiffiesEndTimeStamp = jiffies;
-	unsigned long jiffiesTimeTakenMiliseconds = 1000 * (jiffiesEndTimeStamp -jiffiesStartTimeStamp) / HZ;
+	unsigned long jiffiesTimeTakenMs = 1000 * (jiffies -jiffiesStartTimeStamp) / HZ;
+	//long long kTimeTakenMs = (ktime_get_boottime() - kTimeStartTimeStamp);
 	printk(KERN_ALERT "Goodbye, cruel world\n");
-	printk(KERN_ALERT "Jiffies Time: %li", jiffiesTimeTakenMiliseconds);
+	printk(KERN_ALERT "Jiffies Insertion to Removal Time (ms): %li\n", jiffiesTimeTakenMs);
+	//printk(KERN_ALERT "K_Time Insertion to Removal Time (ms): %lli\n", kTimeTakenMs);
+
 }
 
 module_init(hello_init);
