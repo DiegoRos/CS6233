4fa504799d20309485bb5968c8d81d4e963b5aba
DiegoRos <diegorosenberg@gmail.com>
2024-12-06 13:17:55 -0500

    Added plot and written part of lab 10.

--- assignments/lab10/dr3432_lab10/plotResult.py
+++ assignments/lab10/dr3432_lab10/plotResult.py
@@ -9,13 +9,16 @@ x = data['framesAvailable']
 y = data['pageFault']
 
 # Create the bar chart
-plt.figure(figsize=(10, 6))
-plt.bar(x, y)
+fig, ax = plt.subplots(figsize=(10, 6))
+bars = ax.bar(x, y)
+
+# Add labels to the bars
+ax.bar_label(bars, padding=3, color='black', fontsize=10)
 
 # Set labels and title
 plt.xlabel('Frames Available')
-plt.ylabel('Number of Page Faults')
-plt.title('Lab 10 Results')
+plt.ylabel('Page Faults')
+plt.title('Page Faults vs Frames Available', fontweight='bold',)
 
 # Save the plot as a PNG file
 plt.savefig('lab10_results_chart.png')
